7cda5d929f1dda1ca22b82a5d8320b22bad55e30
Raúl Cátedra <raulcabe3003@gmail.com>
2021-01-10 14:29:51 +0100

    new events done

--- assets/js/events/event-jquery.js
+++ assets/js/events/event-jquery.js
@@ -12,3 +12,28 @@ $("#jquery-event__2-button").on("click", function() {
 $("#jquery-event__3-button").on("dblclick", function() {
   $("#jquery-event__3").append(`<p>Button has been clicked</p>`);
 })
+
+/* User presses a key */
+$(document).on("keypress", function(e) {
+  $("#jquery-event__4").text(`You have pressed ${e.code}.`);
+})
+
+/* User moves the mouse cursor */
+$(window).on("mousemove", function(e) {
+  $("#jquery-event__5").text(`The mouse is in ${e.clientX}x and ${e.clientY}y position.`);
+})
+
+/* User changes a value of an text input */
+$("#jquery-event__6").on("input", function() {
+  $("#jquery-event__6-content").text(`The new value is ${$(this).val()}.`)
+})
+
+/* An image is loaded */
+$("#jquery-event__7-content").on("load", function() {
+  $("#jquery-event__7").append(`<p>The img has been loaded</p>`);
+})
+
+/* An image fails to load */
+$("#jquery-event__8-content").on("error", function() {
+  $("#jquery-event__8").append(`<p>The img can not loaded.</p>`);
+})
--- assets/js/events/event-vanilla.js
+++ assets/js/events/event-vanilla.js
@@ -17,3 +17,37 @@ button2.addEventListener("dblclick", function() {
   const content = document.getElementById("vanilla-event__3");
   content.insertAdjacentHTML("beforeend", "<p>Button has been doble clicked</p>");
 })
+
+/* User presses a key */
+document.addEventListener("keypress", function(e) {
+  const content = document.getElementById("vanilla-event__4");
+  content.innerHTML = `You have pressed ${e.code}.`;
+})
+
+/* User moves the mouse cursor */
+window.addEventListener("mousemove", function(e) {
+  const content = document.getElementById("vanilla-event__5");
+  content.innerHTML = `The mouse is in ${e.clientX}x and ${e.clientY}y position.`
+})
+
+/* User changes a value of an text input */
+const input1 = document.getElementById("vanilla-event__6");
+input1.addEventListener("input", function() {
+  const content = document.getElementById("vanilla-event__6-content");
+  const value = input1.value;
+  content.innerHTML = `The new value is ${value}.`
+})
+
+/* An image is loaded */
+const img1 = document.getElementById("vanilla-event__7-content");
+img1.addEventListener("load", function() {
+  const section = document.getElementById("vanilla-event__7");
+  section.insertAdjacentHTML("beforeend", `<p>The img has been loaded</p>`)
+})
+
+/* An image fails to load */
+const img2 = document.getElementById("vanilla-event__8-content");
+img2.addEventListener("error", function() {
+  const section = document.getElementById("vanilla-event__8");
+  section.insertAdjacentHTML("beforeend", `<p>The img can not loaded.</p>`)
+})
